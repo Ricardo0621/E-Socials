6dc14ed621398b85f138e5c8de3c129b6092c2b8
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2020-11-10 20:52:51 -0500

    Fixed bugs and directories

--- settings.py
+++ settings.py
@@ -8,12 +8,12 @@ SESSION_CONFIGS = [
     #     num_demo_participants=3,
     #     app_sequence=['testapp'],
     # ),
-    dict(
-        name='real_effort_numbers',
-        display_name="Experimento 2 (Real Effort Numbers App) Yes",
-        num_demo_participants=1,
-        app_sequence=['real_effort_numbers'],
-    ),
+    # dict(
+    #     name='real_effort_numbers',
+    #     display_name="Experimento 2 (Real Effort Numbers App) Yes",
+    #     num_demo_participants=1,
+    #     app_sequence=['real_effort_numbers'],
+    # ),
     # dict(
     #     name='myprisoners_dilemma',
     #     display_name="Experimento 3 (Prisoner's Dilemma App) Yes",
@@ -28,7 +28,7 @@ SESSION_CONFIGS = [
     # ),
     dict(
         name='mpl',
-        display_name="Experimento 1",
+        display_name="Efecto de distintos tipos de contratación",
         num_demo_participants=1,
         app_sequence=['mpl'],
     ),
